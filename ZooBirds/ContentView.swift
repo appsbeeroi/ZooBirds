f9881ca3848da956fc26d021accde259d7c1df55
esplusnin <esplusnin94@yandex.ru>
2025-09-08 21:31:41 +0300

    Initial Commit

---
+++ ZooBirds/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZooBirds
+//
+//  Created by Евгений on 08.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZooBirds/ZooBirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZooBirdsApp.swift
+//  ZooBirds
+//
+//  Created by Евгений on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZooBirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
